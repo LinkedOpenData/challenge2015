4016dc84c3c566a27b0cbb35df88ef5eaa4c2f50
Yusuke Komiyama <ykomiyama@bi.a.u-tokyo.ac.jp>
2015-07-29 23:29:09 +0900

    modfied the Web site in concrete5 dir.

---
+++ concrete5/application/config/generated_overrides/concrete.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-07-29T22:54:03+09:00
+ *
+ * @item      misc.latest_version
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'locale' => 'ja_JP',
+    'site' => 'Linked Open Data Challenge 2015',
+    'version_installed' => '5.7.4.2',
+    'misc' => array(
+        'access_entity_updated' => 1438143366,
+        'latest_version' => '5.7.4.2',
+        'do_page_reindex_check' => false,
+        'favicon_fid' => '31',
+        'iphone_home_screen_thumbnail_fid' => '34',
+        'modern_tile_thumbnail_fid' => '33',
+        'modern_tile_thumbnail_bgcolor' => ''
+    ),
+    'permissions' => array(
+        'model' => 'advanced'
+    ),
+    'user' => array(
+        'registration' => array(
+            'email_registration' => false,
+            'type' => 'manual_approve',
+            'captcha' => true,
+            'enabled' => true,
+            'approval' => true,
+            'validate_email' => false,
+            'notification' => '1',
+            'notification_email' => 'komiyama@hgc.jp'
+        )
+    )
+);
